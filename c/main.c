15d9380ee89697da34305c3716565e87f814b64a
Jan Hořava <czechinthemail@gmail.com>
2020-04-12 14:03:02 -0700

    started menu and toolbar

--- c/main.c
+++ c/main.c
@@ -37,10 +37,103 @@ int main(int argc, char **argv){
 }
 
 void startup(GtkApplication* app, gpointer data){
+    GtkAccelGroup *accelgroup;
+    GtkWidget *box;
+    GtkWidget *menu_menu;
+    GtkWidget *menubar;
+    GtkWidget *menuitem_menu;
+    GtkWidget *toolbar;
+
     gtk_init_gtk(
       app,
       "Browser.gtk"
     );
 
+    // Setup scrollable notebook.
+    notebook = GTK_NOTEBOOK(gtk_notebook_new());
+    gtk_notebook_popup_enable(notebook);
+    gtk_notebook_set_scrollable(
+      notebook,
+      TRUE
+    );
+    gtk_notebook_set_show_border(
+      notebook,
+      FALSE
+    );
+    g_signal_connect_after(
+      notebook,
+      "switch-page",
+      G_CALLBACK(tab_switch),
+      NULL
+    );
+
+    // Setup menu items.
+    menubar = gtk_menu_bar_new();
+    accelgroup = gtk_accel_group_new();
+    gtk_window_add_accel_group(
+      GTK_WINDOW(window),
+      accelgroup
+    );
+    // Menu menu.
+    menu_menu = gtk_menu_new();
+    menuitem_menu = gtk_menu_item_new_with_mnemonic("☰");
+    gtk_menu_item_set_submenu(
+      GTK_MENU_ITEM(menuitem_menu),
+      menu_menu
+    );
+    gtk_add_menuitem(
+      menu_menu,
+      "_Quit",
+      accelgroup,
+      KEY_QUIT,
+      GDK_CONTROL_MASK,
+      G_CALLBACK(gtk_widget_destroy),
+      window
+    );
+    gtk_menu_shell_append(
+      GTK_MENU_SHELL(menubar),
+      menuitem_menu
+    );
+
+    // Setup toolbar.
+    toolbar = gtk_box_new(
+      GTK_ORIENTATION_HORIZONTAL,
+      0
+    );
+    gtk_box_pack_start(
+      GTK_BOX(toolbar),
+      GTK_WIDGET(menubar),
+      FALSE,
+      FALSE,
+      0
+    );
+
+    // Add everything to a box.
+    box = gtk_box_new(
+      GTK_ORIENTATION_VERTICAL,
+      0
+    );
+    gtk_box_pack_start(
+      GTK_BOX(box),
+      toolbar,
+      FALSE,
+      FALSE,
+      0
+    );
+    gtk_box_pack_start(
+      GTK_BOX(box),
+      GTK_WIDGET(notebook),
+      TRUE,
+      TRUE,
+      0
+    );
+    gtk_container_add(
+      GTK_CONTAINER(window),
+      box
+    );
+
     gtk_widget_show_all(window);
+}
+
+void tab_switch(GtkNotebook *notebook, GtkWidget *page_content, guint page, gpointer data){
 }
--- c/main.h
+++ c/main.h
@@ -1,4 +1,7 @@
 #pragma once
 
+GtkNotebook *notebook;
+
 void activate(GtkApplication* app, gpointer data);
 void startup(GtkApplication* app, gpointer data);
+void tab_switch(GtkNotebook *notebook, GtkWidget *page_content, guint page, gpointer data);
